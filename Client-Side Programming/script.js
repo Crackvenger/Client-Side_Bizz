8bc9b7d4e55b1acbcafbcb72e174fb43b10fdf6a
Crackvenger <tsukinome123@gmail.com>
2020-01-27 12:44:14 +0800

    Layout design revamp

--- Client-Side Programming/script.js
+++ Client-Side Programming/script.js
@@ -1,53 +1,12 @@
-
-function notify()
-{
-  alert("Thanks for your purchase!")
+function showPrice1() {
+  document.getElementById("hiddenPrice1").style.visibility= 'visible';
 }
-
-function showPrice()
-{
-  document.getElementById("hiddenPrice").style.display = 'block';
+function hidePrice1() {
+  document.getElementById("hiddenPrice1").style.visibility= 'hidden';
 }
-
-const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-
-const hexBtn = document.querySelector('.hexBtn');
-
-const bodyBCG = document.querySelector('body');
-
-const hex = document.querySelector('.hex');
-
-hexBtn.addEventListener('click',getHex);
-
-function getHex() {
-	let hexCol = '#';
-	for (let i = 0; i < 6; i++) {
-		let random = Math.floor(Math.random()*hexNumbers.length);
-		//console.log(random);
-		hexCol += hexNumbers[random];
-		//console.log(hexCol)
-	}
-	bodyBCG.style.backgroundColor = hexCol;
-	hex.innerHTML = hexCol;
+function showPrice2() {
+  document.getElementById("hiddenPrice2").style.visibility= 'visible';
 }
-/*function GetDateDemo()
-{
-var myDate = new Date();
-var year = myDate.getFullYear();
-var month = myDate.getMonth();
-var date = myDate.getDate();
-var hours = myDate.getHours();
-var minutes = myDate.getMinutes();
-var seconds = myDate.getSeconds();
-
-if(month < 10 ){
-  month = "0" + month;
+function hidePrice2() {
+  document.getElementById("hiddenPrice2").style.visibility= 'hidden';
 }
-if(date < 10 ){
-  date = "0" + date;
-}
-
-var dateTime = year + "." + month + "." + date "." + hour + ":" minutes + "." + seconds;
-var divNode = document.getElementById("time");
-divNode.innerHTML = dateTime;
-}*/
---
+++ Client-side.js
@@ -0,0 +1,53 @@
+
+function notify()
+{
+  alert("Thanks for your purchase!")
+}
+
+function showPrice()
+{
+  document.getElementById("hiddenPrice").style.display = 'block';
+}
+
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+const hexBtn = document.querySelector('.hexBtn');
+
+const bodyBCG = document.querySelector('body');
+
+const hex = document.querySelector('.hex');
+
+hexBtn.addEventListener('click',getHex);
+
+function getHex() {
+	let hexCol = '#';
+	for (let i = 0; i < 6; i++) {
+		let random = Math.floor(Math.random()*hexNumbers.length);
+		//console.log(random);
+		hexCol += hexNumbers[random];
+		//console.log(hexCol)
+	}
+	bodyBCG.style.backgroundColor = hexCol;
+	hex.innerHTML = hexCol;
+}
+/*function GetDateDemo()
+{
+var myDate = new Date();
+var year = myDate.getFullYear();
+var month = myDate.getMonth();
+var date = myDate.getDate();
+var hours = myDate.getHours();
+var minutes = myDate.getMinutes();
+var seconds = myDate.getSeconds();
+
+if(month < 10 ){
+  month = "0" + month;
+}
+if(date < 10 ){
+  date = "0" + date;
+}
+
+var dateTime = year + "." + month + "." + date "." + hour + ":" minutes + "." + seconds;
+var divNode = document.getElementById("time");
+divNode.innerHTML = dateTime;
+}*/
