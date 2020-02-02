ea7d4a5ed6329f65af5a3fc5db82bd94c2d2136f
Crackvenger <tsukinome123@gmail.com>
2020-02-02 17:08:53 +0800

    trying to make switching ads div

--- Client-Side Programming/script.js
+++ Client-Side Programming/script.js
@@ -1,5 +1,6 @@
-var picture = [];
-
+var picture = ["bestad.jpg", "Converse-Shoe.png", "East-Side-Games-IMG_2190.jpg"];
+var msec=0;
+var i=0;
 function showPrice1() {
   document.getElementById("hiddenPrice1").style.visibility= 'visible';
 }
@@ -12,4 +13,10 @@ function showPrice2() {
 function hidePrice2() {
   document.getElementById("hiddenPrice2").style.visibility= 'hidden';
 }
-function 
+function slide() {
+msec++;
+if(msec==1000){
+  msec=0;
+  i++;
+}
+document.
